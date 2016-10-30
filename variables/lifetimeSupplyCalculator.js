31f2aee8efe17144e0bd46b0773f925201b13de9
Nour <nouraswad@hotmail.com>
2016-10-30 17:23:21 +0000

    Update lifetimeSupplyCalculator.js

--- variables/lifetimeSupplyCalculator.js
+++ variables/lifetimeSupplyCalculator.js
@@ -1,1 +1,6 @@
-// Enter code below
+var age=29;
+var max_age=100;
+var amount=3;
+var y= max_age-age;
+var z= 3*y*360;
+console.log('You will need '+z+' to last you until the ripe old age of '+max_age);
