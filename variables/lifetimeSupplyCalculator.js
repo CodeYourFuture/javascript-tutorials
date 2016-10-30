d362b0350e51f27f3fb91d1758fd2c2c05342259
Bridget Kalinda <bridgetkalinda@gmail.com>
2016-10-30 17:10:52 +0000

    completed all the exercises

--- variables/lifetimeSupplyCalculator.js
+++ variables/lifetimeSupplyCalculator.js
@@ -1,1 +1,7 @@
 // Enter code below
+var currentAge = 24;
+var maxAge = 100;
+var estAmount = 2;
+var total = maxAge * estAmount
+
+" You will need" + total + " until the ripe old age of" + maxAge + " ."
