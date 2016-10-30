31a84830afd700e14b7f3a7e712a3bd5180ff5cc
ArockiaAnsi <arockiaansi@gmail.com>
2016-10-30 15:22:04 +0000

    Add the JS codes

--- variables/ageCalculator.js
+++ variables/ageCalculator.js
@@ -1,1 +1,5 @@
 // Enter code below
+var b_year= 1989;
+var f_year= 2017;
+var age = f_year-b_year;
+console.log("I will be either "+age+ " or " +(age+1)+ " in " +f_year); 
--- variables/lifetimeSupplyCalculator.js
+++ variables/lifetimeSupplyCalculator.js
@@ -1,1 +1,9 @@
 // Enter code below
+var c_age = 29;
+
+var m_age = 125;
+
+var amount = 3;
+
+
+console.log ("I will need " + ((m_age - c_age) * 3)  + " to last me until the ripe age of " + m_age);
