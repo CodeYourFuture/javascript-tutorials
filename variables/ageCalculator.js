959c6e22884d9f73437d2a674d2a165473f8cb23
ArockiaAnsi <arockiaansi@gmail.com>
2016-11-11 15:06:05 +0000

    Edit the JS codes

--- variables/ageCalculator.js
+++ variables/ageCalculator.js
@@ -1,5 +1,5 @@
 // Enter code below
-var b_year= 1989;
-var f_year= 2017;
-var age = f_year-b_year;
-console.log("I will be either "+age+ " or " +(age+1)+ " in " +f_year); 
+var birthYear = 1989;
+var futureYear = 2017;
+var age = futureYear - birthYear;
+console.log("I will be either "+ age + " or " +(age - 1)+ " in " + futureYear); 
--- variables/fortuneTeller.js
+++ variables/fortuneTeller.js
@@ -1,13 +1,7 @@
 // Enter code below
 var n_children = 2;
-
 var name = "Luis";
-
 var city = "London";
-
 var job = "coder";
-
 var p_name = "Sally";
-
-
 console.log("I am " + name + " in" + city + ", and married to" + p_name + "with" + n_children + "kids");
